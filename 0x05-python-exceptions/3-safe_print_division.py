d9a173506e806f2caff75b3d326a9572e818de66
jeanvictoria <jeanvictoria>
2019-09-30 12:26:19 -0500

    Solution to point three

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+ 
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        b = "{:.1f}".format(a / b)
+        return b
+    except:
+        b = "None"
+        return None
+    finally:
+        print("Inside result: {}".format(b))
