219b74ea0543dd46ab4e96df990c7837caa67932
jean.victoriaclear <jean.victoria>
2020-03-06 12:25:19 -0500

    Correction to task

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """
-This module contains a function to add two numbers
+Module add-integer
+Adds two integer together
 """
 
 
 def add_integer(a, b=98):
+    """Returns the addition of a and b,
+    or an error if a and b are not integers or floats
     """
-    Add two numbers
-    Parameters:
-    @a: Number1
-    @b: Number2
-    Return: Sum of two numbers
-    """
+
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+
+    return a + b
