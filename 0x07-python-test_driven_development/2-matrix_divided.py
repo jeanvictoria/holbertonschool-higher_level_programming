91a92a09dac408010f71661e6c404d6b088b4b11
jean.victoriaclear <jean.victoria>
2020-03-06 12:40:00 -0500

    Correction to task

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,46 +1,41 @@
 #!/usr/bin/python3
 """
-This module contains a function todivide each element
+Module matrix_divided
+Divides each element of a matrix of numbers by a number
 """
 
 
 def matrix_divided(matrix, div):
+    """Returns a new matrix (list of list)
+    with the result of the division of matrix by div
+    rounded to 2 decimal places.
     """
-    Function to divide each element by div
-    @matrix: matrix
-    @div: number to divide
-    Return: New matrix
-    """
-    msg = "matrix must be a matrix (list of lists) of integers/floats"
-    if type(div) is not int and type(div) is not float:
-        raise TypeError("div must be a number")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-    if type(matrix) is not list or type(matrix[0]) is not list:
-        raise TypeError(msg)
-    new_matrix = []
+
+    if not isinstance(matrix, list) or len(matrix) == 0 or not matrix[0]:
+        raise TypeError("matrix must be a matrix (list of lists) " +
+                        "of integers/floats")
+
+    for row in matrix:
+        if len(row) == 0:
+            raise TypeError("matrix must be a matrix (list of lists) " +
+                            "of integers/floats")
+        for x in row:
+            if type(x) is not int and type(x) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) " +
+                                "of integers/floats")
+
     len_rows = []
     for row in matrix:
         len_rows.append(len(row))
-        if (len(row) != average_list(len_rows)):
-            raise TypeError("Each row of the matrix must have the same size")
-        new_row = []
-        for elem in row:
-            if type(elem) is not int and type(elem) is not float:
-                raise TypeError(msg)
-            else:
-                new_row.append(round((elem / div), 2))
-        new_matrix.append(new_row)
+    if not all(elem == len_rows[0] for elem in len_rows):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new_matrix = [[round(x / div, 2) for x in row] for row in matrix]
+
     return new_matrix
-
-
-def average_list(list_i):
-    """
-    Funcion to calculate average of list
-    @list: list
-    Return: Average of list
-    """
-    average = 0
-    for item in list_i:
-        average += item
-    return average / len(list_i)
