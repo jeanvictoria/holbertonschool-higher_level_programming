6321ce0f78352f5d56211c6e42ad7a73a6965fdf
jean.victoriaclear <jean.victoria>
2020-03-07 18:17:39 -0500

    Solution to task two

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-if (process.argv<1){
-  	console.log("No argument");
-} else if {(process.argv>1)
-	console.log("Arguments found");
-} else { 
-	console.log("Argument found");
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
