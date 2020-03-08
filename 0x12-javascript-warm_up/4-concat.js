67857291bacedf3964fe76a2687daa41a6036949
jean.victoriaclear <jean.victoria>
2020-03-07 19:13:25 -0500

    Solution to task four

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const arguments1 = process.argv[2];	
+const arguments1 = process.argv[2];
 const arguments2 = process.argv[3];
-const concat = `${arguments1} is ${arguments2}` ; 
+const concat = `${arguments1} is ${arguments2}`;
 
-  console.log(concat);
+console.log(concat);
