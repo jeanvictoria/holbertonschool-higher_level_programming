09f70aa57ac744da5df3d012afd819c261bacefe
jean.victoriaclear <jean.victoria>
2020-03-08 10:48:57 -0500

    Correction to task eleven

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
-const argms = process.argv[2];
-if (argms.length <= 1) {
-	console.log(0);
-} else { 
-	const nums = argms.map(value => parseInt(value));
-	const max = Math.max(...nums);
-	const fill = [];
-	for (let i = 0; i < nums.length; i++) { 
-		if (nums[i] !== max) {
-			fill.push(nums[i]);
-		}
-	}
-	const secondMax = Math.max(...fill);
-	console.log(secondMax);
+if (process.argv.length <= 3) {
+  console.log('0');
+} else {
+  const array = process.argv.slice(2).map(Number);
+  const secondMax = array.sort(function (a, b) { return b - a; })[1];
+  console.log(secondMax);
 }
