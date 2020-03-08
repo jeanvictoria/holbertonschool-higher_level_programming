4f05768b3092074d5bdda10a7f1d8cfa1244c189
jean.victoriaclear <jean.victoria>
2020-03-07 19:35:22 -0500

    Solution to task six

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const languages = ['C is fun', 'Python is cool', 'Javasript is amazing'];
+const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 languages.forEach(element => console.log(element));
