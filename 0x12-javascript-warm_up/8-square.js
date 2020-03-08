401be62d8d669b8d2e9734c899082c505fab9557
jean.victoriaclear <jean.victoria>
2020-03-08 10:45:34 -0500

    Correction task eigth

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  const size = process.argv[2];
-
-  for (let i = 0; i < size; i++) {
-    console.log('X'.repeat(size));
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('X'.repeat(x));
+    i++;
   }
 }
